a99372a345db9cf1e37f78c924b3ba0183859f75
CaahSilva <camila100rbd@gmail.com>
2015-05-25 14:52:33 -0300

    Buscas Snippets [FIX]

---
+++ Projeto.Dados/Controllers/ArquivoController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Projeto.Dados.DAL;
+using Projeto.Dados.Models;
+using System.IO;
+using System.Text;
+
+namespace Projeto.Dados.Controllers
+{
+    public class ArquivoController : ApiController
+    {
+        private projetoDBContext db = new projetoDBContext();
+        private IComponenteRepository snippetRepository;
+
+        public ArquivoController()
+        {
+            this.snippetRepository = new ComponenteRepository(this.db);
+        }
+
+        // Retorna os arquivos -- OK
+        public string[] Get(int id)
+        {
+
+            List<string> result = new List<string>();
+
+            //Verifica se existe um snippet com esse id
+            if (this.snippetRepository.GetComponenteByID(id) != null)
+            {
+
+                string path = @"C:\Users\manoela.OPUS\Documents\Projeto_Final_Estagio\RepositorioOpus\Projeto.Dados\Arquivos\Codigos\" + id.ToString();
+
+                // Verifica se o arquivo html existe
+                if (File.Exists(path + "\\arq.html"))
+                {
+                    result.Add(File.ReadAllText(path + "\\arq.html"));
+                }
+                else
+                {
+                    result.Add(null);
+                }
+
+                // Verifica se o arquivo css existe
+                if (File.Exists(path + "\\arq.css"))
+                {
+                    result.Add(File.ReadAllText(path + "\\arq.css"));
+                }
+                else
+                {
+                    result.Add(null);
+                }
+
+                // Verifica se o arquivo js existe
+                if (File.Exists(path + "\\arq.js"))
+                {
+                    result.Add(File.ReadAllText(path + "\\arq.js"));
+                }
+                else
+                {
+                    result.Add(null);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
--- Projeto.Dados/Controllers/SnippetController.cs
+++ Projeto.Dados/Controllers/SnippetController.cs
@@ -6,9 +6,6 @@ using System.Net.Http;
 using System.Web.Http;
 using Projeto.Dados.Models;
 using Projeto.Dados.DAL;
-using System.IO;
-using System.Text;
-using System.Net.Http.Headers;
 
 namespace Projeto.Dados.Controllers
 {
@@ -23,14 +20,11 @@ namespace Projeto.Dados.Controllers
             this.snippetRepository = new ComponenteRepository(this.db);
         }
 
-        // Retorna a url das imagens de todos os snippets contidos no banco
-        public String[] Get()
-        {
+        // Monta os endereços de um conjunto de imagens
+        private String[] getEndImagens(List<Componente> snippets){
 
-            List<Componente> snippets = this.snippetRepository.GetComponentes().ToList();
-
-            string caminhoServer = System.Web.HttpContext.Current.Request.Url.Host + ":" + System.Web.HttpContext.Current.Request.Url.Port + "/Arquivos/Imagens/";
             List<string> retorno = new List<string>();
+            string caminhoServer = System.Web.HttpContext.Current.Request.Url.Host + ":" + System.Web.HttpContext.Current.Request.Url.Port + "/Arquivos/Imagens/";
 
             foreach (Componente snippet in snippets)
             {
@@ -38,10 +32,20 @@ namespace Projeto.Dados.Controllers
             }
 
             return retorno.ToArray();
+
         }
 
-        // Retorna a url das imagens de todos os snippets contidos no banco, filtrados por titulo e keyword
-        [Route("api/snippet/busca/{nome}/{idProjeto}")]
+
+        // Retorna a url das imagens de todos os snippets contidos no banco -- OK
+        public String[] Get()
+        {
+            List<Componente> snippets = this.snippetRepository.GetComponentes().ToList();
+
+            return getEndImagens(snippets);
+        }
+
+        // Retorna a url das imagens de todos os snippets contidos no banco, filtrados por titulo e keyword -- OK
+        [Route("api/snippet/busca/{nome}")]
         public String[] Get(string nome)
         {
             string nomeMin = nome.ToLower();
@@ -51,19 +55,10 @@ namespace Projeto.Dados.Controllers
                 .Where(c => c.nome.ToLower().Contains(nomeMin) || 
                     c.Keyword.Any(k => k.kw.ToLower().Contains(nomeMin))).ToList();
 
-
-            string caminhoServer = System.Web.HttpContext.Current.Request.Url.Host + ":" + System.Web.HttpContext.Current.Request.Url.Port + "/Arquivos/Imagens/";
-            List<string> retorno = new List<string>();
-
-            foreach (Componente snippet in snippets)
-            {
-                retorno.Add(caminhoServer + snippet.idComponente.ToString() + ".png");
-            }
-
-            return retorno.ToArray();
+            return getEndImagens(snippets);
         }
 
-        // Retorna a url das imagens de todos os snippets contidos no banco, filtrados por titulo, kw e projeto
+        // Retorna a url das imagens de todos os snippets contidos no banco, filtrados por titulo, kw e projeto -- OK
         [Route("api/snippet/busca/{nome}/{idProjeto}")]
         public String[] GetByProjeto(string nome, int idProjeto)
         {
@@ -75,87 +70,23 @@ namespace Projeto.Dados.Controllers
                 .Where(c => c.projeto == idProjeto && (c.nome.ToLower().Contains(nomeMin)) ||
                     c.Keyword.Any(k => k.kw.ToLower().Contains(nomeMin))).ToList();
 
-            
-            string caminhoServer = System.Web.HttpContext.Current.Request.Url.Host + ":" + System.Web.HttpContext.Current.Request.Url.Port + "/Arquivos/Imagens/";
-            List<string> retorno = new List<string>();
-
-            foreach (Componente snippet in snippets)
-            {
-                retorno.Add(caminhoServer + snippet.idComponente.ToString() + ".png");
-            }
-
-            return retorno.ToArray();
+            return getEndImagens(snippets);
         }
 
-        // Retorna a url das imagens de todos os snippets contidos no banco, filtrados por projeto
-        [Route("api/snippet/busca/{idProjeto}")]
+        // Retorna a url das imagens de todos os snippets contidos no banco, filtrados por projeto -- OK
+        [Route("api/snippet/busca/{idProjeto:int}")]
         public String[] GetByProjeto(int idProjeto)
         {
 
             List<Componente> snippets = this.snippetRepository.GetComponentes().Where(c => c.projeto == idProjeto).ToList();
 
-            string caminhoServer = System.Web.HttpContext.Current.Request.Url.Host + ":" + System.Web.HttpContext.Current.Request.Url.Port + "/Arquivos/Imagens/";
-            List<string> retorno = new List<string>();
-
-            foreach (Componente snippet in snippets)
-            {
-                retorno.Add(caminhoServer + snippet.idComponente.ToString() + ".png");
-            }
-
-            return retorno.ToArray();
+            return getEndImagens(snippets);
         }
 
-        // Pega todas as informações de um snippet específico
+        // Pega todas as informações de um snippet específico -- OK
         public Componente Get(int id)
         {
             return this.snippetRepository.GetComponenteByID(id);
-        }
-
-        // Retorna os arquivos
-        [Route("api/snippet/{id}/files")]
-        public string[] GetFiles(int id)
-        {
-
-            List<string> result = new List<string>();
-
-            //Verifica se existe um snippet com esse id
-            if (this.snippetRepository.GetComponenteByID(id) != null)
-            {
-
-                string path = @"C:\Users\manoela.OPUS\Documents\Projeto_Final_Estagio\RepositorioOpus\Projeto.Dados\Arquivos\Codigos\" + id.ToString();
-
-                // Verifica se o arquivo html existe
-                if (File.Exists(path + "\\arq.html"))
-                {
-                    result.Add(File.ReadAllText(path + "\\arq.html"));
-                }
-                else
-                {
-                    result.Add(null);
-                }
-
-                // Verifica se o arquivo css existe
-                if (File.Exists(path + "\\arq.css"))
-                {
-                    result.Add(File.ReadAllText(path + "\\arq.css"));
-                }
-                else
-                {
-                    result.Add(null);
-                }
-
-                // Verifica se o arquivo js existe
-                if (File.Exists(path + "\\arq.js"))
-                {
-                    result.Add(File.ReadAllText(path + "\\arq.js"));
-                }
-                else
-                {
-                    result.Add(null);
-                }
-            }
-
-            return result.ToArray();
         }
     }
 }
