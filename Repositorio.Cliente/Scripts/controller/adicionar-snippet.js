df90524b02fca1ce440ac0f8ce5006eef51d9d7b
Felipe Augusto do Amaral <amaral.felipeaugusto@gmail.com>
2015-05-27 15:34:53 -0300

    Layout Atualizado!

--- Repositorio.Cliente/Scripts/controller/adicionar-snippet.js
+++ Repositorio.Cliente/Scripts/controller/adicionar-snippet.js
@@ -1,34 +1,6 @@
-﻿app.controller('adicionarSnippetCtrl', function ($scope, $timeout, $q) {
-    $scope.DemoCtrl = function ($timeout, $q) {
-        var self = this;
-
-        self.readonly = false;
-
-        // Lists of fruit names and Vegetable objects
-        self.fruitNames = ['Apple', 'Banana', 'Orange'];
-        self.roFruitNames = angular.copy(self.fruitNames);
-        self.tags = [];
-        self.vegObjs = [
-          {
-              'name': 'Broccoli',
-              'type': 'Brassica'
-          },
-          {
-              'name': 'Cabbage',
-              'type': 'Brassica'
-          },
-          {
-              'name': 'Carrot',
-              'type': 'Umbelliferous'
-          }
-        ];
-
-        self.newVeg = function (chip) {
-            return {
-                name: chip,
-                type: 'unknown'
-            };
-        };
-    }
-
+﻿app.controller('chipsCtrl', function ($scope, $timeout, $q) {
+    var self = this;
+    self.tags = [];
 });
+
+
