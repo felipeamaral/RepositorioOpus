60be9f5fb008f87d1e2c27f6ad0cc926b2e9d1c4
CaahSilva <camila100rbd@gmail.com>
2015-05-28 16:33:00 -0300

    Paginação ok
    Finalização da paginação da tela de buscas do snippet

--- Repositorio.Cliente/Scripts/controller/SnippetsCtrl.js
+++ Repositorio.Cliente/Scripts/controller/SnippetsCtrl.js
@@ -1,28 +1,85 @@
 ﻿app.controller('SnippetsCtrl', function ($scope, apiService) {
 
-    var qntd = 4;
+    var qntd = 2;
+    var qntdPaginasMostra = 5;
+
+    $scope.qntdPaginas = 0;
+    $scope.paginaAtual = 1;
 
     /*Pega os snippets da página informada*/
-    $scope.goToPage = function (pageNumber) {
+    $scope.goToPage = function (pageNumber, primeira, last) {
         var aux = apiService.getImagens.query({ qntd: qntd, pageNumber: pageNumber }, function () {
+
+            /*Verifica se clicou pra ir direto pra primeira página*/
+            if (primeira) {
+                for (i = 0; i < qntdPaginasMostra && i < $scope.qntdPaginas; i++) {
+                    $scope.paginas[i] = i+1;
+                }
+            }
+
+            /*Verifica se clicou pra ir direto pra ultima pagina*/
+            if (last) {
+                cont = 0;
+                for (i = $scope.qntdPaginas - qntdPaginasMostra >= 0 ? $scope.qntdPaginas - qntdPaginasMostra + 1 : 1; i <= $scope.qntdPaginas; i++) {
+                    $scope.paginas[cont] = i;
+                    cont++;
+                }
+            }
+
             $scope.snippets = aux;
+            $scope.paginaAtual = pageNumber;
         });
     };
 
     /*Pega os snippets da página 1*/
     $scope.goToPage(1);
 
+    /*Vetor com numeração das páginas a serem mostradas*/
+    $scope.paginas = [];
+
     /*Função que faz o download dos códigos do componente*/
     $scope.download = function () { };
+    
 
-    /*Quantidade de páginas no total*/
-    $scope.paginas = [];
+    /*Inicializa as páginas a serem mostradas*/
     var q = apiService.getImagens.query(function () {
-        var qntdPaginas = q % qntd > 0 ? Math.floor(q / qntd) + 1 : Math.floor(q / qntd);
-        for (var i = 0; i < qntdPaginas; i++){
+        $scope.qntdPaginas  = q % qntd > 0 ? Math.floor(q / qntd) + 1 : Math.floor(q / qntd);
+        for (var i = 0; i < qntdPaginasMostra && i < $scope.qntdPaginas; i++){
             $scope.paginas[i] = i + 1;
         }
     });
-    
-    
+
+    /*Vai pra próxima pagina*/
+    $scope.goToNextPage = function () {
+
+        /*Verifica se existem páginas a serem mostradas*/
+        if ($scope.paginaAtual < $scope.qntdPaginas) {
+            $scope.goToPage($scope.paginaAtual + 1);
+
+            /*Ta na ultima pagina que ta sendo mostrada*/
+            if ($scope.paginaAtual == $scope.paginas[qntdPaginasMostra - 1]) {
+                
+                for (i = 0; i < qntdPaginasMostra; i++) {
+                    $scope.paginas[i] = $scope.paginas[i] + 1;
+                }
+            }
+        }
+    }
+
+    /*Vai para a pagina anterior*/
+    $scope.goToPreviousPage = function () {
+
+        /*Verifica se existem páginas a serem mostradas*/
+        if ($scope.paginaAtual > 1) {
+            $scope.goToPage($scope.paginaAtual - 1);
+
+            /*Ta na ultima primeira que ta sendo mostrada*/
+            if ($scope.paginaAtual == $scope.paginas[0]) {
+
+                for (i = 0; i < qntdPaginasMostra; i++) {
+                    $scope.paginas[i] = $scope.paginas[i] - 1;
+                }
+            }
+        }
+    }
 });
