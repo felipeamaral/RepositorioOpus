e5a5031bfbe9f9693765cae89f58c90ab24d2f27
Felipe Augusto do Amaral <amaral.felipeaugusto@gmail.com>
2015-05-28 15:49:56 -0300

    Adicionar-Snippet
    Começo do desenvolvimento com o Angular.
    Correção da apresentação das Tags.

--- Repositorio.Cliente/Scripts/controller/adicionar-snippet.js
+++ Repositorio.Cliente/Scripts/controller/adicionar-snippet.js
@@ -1,7 +1,27 @@
-﻿app.controller('chipsCtrl', DemoCtrl);
-function DemoCtrl($timeout, $q) {
+﻿app.controller('adicionar-snippet', function ($timeout, $q, $scope) {
     var self = this;
     self.readonly = false;
     self.tags = [];
-   
-}
+
+    $scope.snippet = {
+        Titulo: '',
+        Projeto: '',
+        tags: ''
+    }
+
+    $scope.salvaSnippet = function () {
+        console.log($scope.snippet.Titulo);
+        console.log($scope.snippet.Projeto);
+        console.log(self.tags);   
+    }
+
+    //Função que pega os dados do editor e manda em um arquivo pro servidor
+    function upload(conteudo) {
+        $.post("http://localhost:53412/api/snippet/3/files/upload", function () {
+            var file = [conteudo];
+            var blob = new Blob(file, { type: 'application/octet-binary' }); // the blob
+            console.log(blob);
+        });
+    };
+
+});
