feefc6d7db7a6387ac717e2dbd36e2faef829145
CaahSilva <camila100rbd@gmail.com>
2015-06-10 16:31:58 -0300

    Renderização ok
    Renderizando todo o código do editor, inclusive o js, e mostrando o
    resultado na visualização

--- Repositorio.Cliente/repositorio/controllers/VisualizacaoCtrl.js
+++ Repositorio.Cliente/repositorio/controllers/VisualizacaoCtrl.js
@@ -6,7 +6,11 @@ visualizacao.controller('VisualizacaoCtrl', function ($scope, $rootScope) {
     window.atualiza = function () {
         document.getElementById("style").innerHTML = window.parent.angular.element(window.frameElement).scope().aceSessions[1].getValue();
         document.getElementById("body").innerHTML = window.parent.angular.element(window.frameElement).scope().aceSessions[0].getValue();
-        document.getElementById("script").innerHTML = window.parent.angular.element(window.frameElement).scope().aceSessions[2].getValue();
+
+
+        var script = document.createElement('script');
+        script[(script.innerText === undefined ? "textContent" : "innerText")] = window.parent.angular.element(window.frameElement).scope().aceSessions[2].getValue();
+        document.documentElement.appendChild(script);
     }
 
 });
